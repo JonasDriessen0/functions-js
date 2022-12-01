ca50302dad9c7b956c2e290e0a5ea024868b159d
Jonas Driessen <30167@ma-web.nl>
2022-12-01 10:51:05 +0100

    aids

---
+++ app.js
@@ -0,0 +1,30 @@
+class App
+{
+    func1()
+    {
+        console.log("MARIO!!")
+    }
+
+    func2()
+    {
+        let var1 = "BWAHAHA!!!"
+        return var1
+    }
+
+    func3()
+    {
+        let num1 = 13 + 6
+        return num1
+    }
+
+    func4()
+    {
+        let var2 = true
+        return var2
+    }
+}
+let app = new App();
+app.func1();
+app.func2();
+app.func3();
+app.func4();
